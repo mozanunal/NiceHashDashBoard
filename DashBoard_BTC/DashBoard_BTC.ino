5ac65c653b15f59af6b544c613eaea086c223a28
mozanunal <mehmetozanunal@gmail.com>
2017-06-18 20:30:47 +0300

    first working code it is get data and show it with scroll

--- DashBoard_BTC/DashBoard_BTC.ino
+++ DashBoard_BTC/DashBoard_BTC.ino
@@ -5,31 +5,35 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 
-ESP8266WiFiMulti WiFiMulti;
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
 
+//User Settings
+const char ssid_1 [] = "xx";
+const char pass_1 [] = "xx";
+const char BitCoinWallet [] = "xx";
 
-const char ssid_1 [] = "BABALAR";
-const char pass_1 [] = "24939300";
-const char BitCoinWallet [] = "1FVJWQaaobMvr9omaRo566xRP5BJQAkE4k";
-char url_Currency [] = "http://api.nicehash.com/api?method=stats.global.current";
+//Globals
+char url_Currency [] = "http://blockchain.info/ticker";
+char url_Balance_base[] = "http://api.nicehash.com/api?method=stats.provider&addr=";
 char url_Balence[255];
+ESP8266WiFiMulti WiFiMulti;
+SSD1306  display(0x3c, D3, D5);
 
 
-
-
-void setup() {
+void setup() 
+{
   
     //Setup Serial
     Serial.begin(115200);
     delay(10);
     
     //Setup URLs
-    char* url = "https://api.nicehash.com/api?method=stats.provider&addr=";
-    strcpy( url_Balence, url );
+    strcpy( url_Balence, url_Balance_base );
     strcat( url_Balence, BitCoinWallet );
     Serial.println();
-    Serial.println(url_Balence);
 
     //Setup Wifi
     WiFiMulti.addAP( ssid_1 , pass_1);
@@ -47,33 +51,116 @@ void setup() {
     delay(500);
 
     //Setup Oled
-
+    display.init();
+    display.flipScreenVertically();
+    display.setFont(ArialMT_Plain_16);
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.clear();
     
 }
 
-
-void loop() {
-
-  if (WiFiMulti.run() == WL_CONNECTED) { //Check WiFi connection status
- 
+String getJsonFromUrl(char *url)
+{
     HTTPClient http;  //Declare an object of class HTTPClient
- 
-    http.begin(url_Currency);  //Specify request destination
+    String payload;
+    http.begin(url);  //Specify request destination
     int httpCode = http.GET();                                                                  //Send the request
- 
-    if (httpCode > 0) { //Check the returning code
- 
-      String payload = http.getString();   //Get the request response payload
-      Serial.println(payload);                     //Print the response payload
- 
+    if (httpCode > 0) //Check the returning code
+    { 
+      payload = http.getString();   //Get the request response payload
+      Serial.println(payload);      //Print the response payloa
     }
- 
     http.end();   //Close connection
- 
-  }
- 
-  delay(30000);    //Send a request every 30 seconds
+    return payload;
 }
+
+void printBuffer(double currency, double totol_balance_btc, double total_balance ) 
+{
+  // Initialize the log buffer
+  // allocate memory to store 3 lines of text and 30 chars per line.
+  display.setLogBuffer(3, 30);
+
+  char line1[30];
+  char line2[30];
+  char line3[30];
+
+
+  dtostrf(currency,0, 8, line1);
+  dtostrf(totol_balance_btc,0, 8, line2);
+  dtostrf(total_balance,0, 8, line3);
+  Serial.println(line1);
+
+  // Some test data
+  char* test[] = {
+    line1,
+    line2,
+    line3
+  };
+
+  for (uint8_t i = 0; i < 3; i++) {
+    display.clear();
+    // Print to the screen
+    display.println(test[i]);
+    // Draw it to the internal screen buffer
+    display.drawLogBuffer(0, 0);
+    // Display it on the screen
+    display.display();
+    delay(500);
+  }
+}
+
+DynamicJsonBuffer jsonBuffer_Currency;
+DynamicJsonBuffer jsonBuffer_Balance;
+
+void loop() 
+{
+
+  if (WiFiMulti.run() == WL_CONNECTED) //Check WiFi connection status
+  { 
+
+
+    //Currency
+    JsonObject& root_Currency = jsonBuffer_Currency.parseObject( getJsonFromUrl(url_Currency) );
+    double currency = root_Currency["USD"]["last"];
+
+    for (int i = 0; i < 150; i++ )
+    {
+      //Balance
+      delay(5000);
+      JsonObject& root_Balance = jsonBuffer_Balance.parseObject( getJsonFromUrl(url_Balence) );
+  
+      double balances[6];
+      int algos[6];
+      double total_balance_btc = 0.0;
+      for (int i = 0; i<6; i++)
+      {
+          algos[i] = root_Balance["result"]["stats"][i]["algo"];
+          const char* balchar = root_Balance["result"]["stats"][i]["balance"];
+          balances[i] = strtod(balchar, NULL);
+          //Serial.println(algos[i]);
+          //Serial.println(balchar);
+          //Serial.println(balances[i],8);
+          total_balance_btc += balances[i];
+      }
+      
+      Serial.print("1 BTC: ");
+      Serial.print(currency,9);
+      Serial.println(" USD");
+      Serial.print("Total BTC: ");
+      Serial.println(total_balance_btc,9);
+      Serial.print("Total USD: ");
+      Serial.println(total_balance_btc*currency,9);
+      printBuffer(currency, total_balance_btc, total_balance_btc*currency);
+    }
+
+
+  }
+  
+}
+
+
+
+
 
 
 
